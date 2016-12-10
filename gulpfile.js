36cf7672f54cc820932271ead509efe66b5a0e2e
ucozlab <artem@ucoz-team.net>
2016-12-10 12:10:22 +0200

    project start

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+var gulp         = require('gulp'),
+	less         = require('gulp-less'),
+	watch        = require('gulp-watch'),
+	cleanCSS     = require('gulp-clean-css'),
+	concat       = require('gulp-concat'),
+	autoprefixer = require('gulp-autoprefixer'),
+	uglify       = require('gulp-uglifyjs'),
+	paths        = {
+		scripts : [
+			'./layout/js/src/jquery.min.js',
+			'./layout/js/src/chart.js',
+			'./node_modules/bxslider/dist/jquery.bxslider.min.js',
+			'./layout/js/src/main.js'
+		]
+	};
+
+gulp.task('compile-less', function() {
+	gulp.src('./layout/less/style.less')
+		.pipe(less())
+		.pipe(autoprefixer({
+			browsers: ['last 5 versions'],
+			cascade: false
+		}))
+		// .pipe(cleanCSS({advanced : false}))
+		.pipe(gulp.dest('./layout/css/'));
+});
+
+gulp.task('watch-less', function() {  
+	gulp.watch('./layout/less/*.less', ['compile-less']);
+});
+
+gulp.task('compile-js', function() {
+	gulp.src(paths.scripts)
+		.pipe(concat('scripts.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('./layout/js/'));
+});
+
+gulp.task('watch-js', function() {
+	gulp.watch(paths.scripts, ['compile-js']);
+});
+
+gulp.task('default', ['compile-less', 'watch-less', 'compile-js', 'watch-js'] );  
