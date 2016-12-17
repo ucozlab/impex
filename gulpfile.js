4a5acba3de94e0deea4370ca4818128bda2147f3
ucozlab <la-3111@mail.ru>
2016-12-17 18:34:14 +0200

    main page ready

--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,7 @@ var gulp         = require('gulp'),
 	watch        = require('gulp-watch'),
 	cleanCSS     = require('gulp-clean-css'),
 	concat       = require('gulp-concat'),
-	autoprefixer = require('gulp-autoprefixer'),
-	uglify       = require('gulp-uglifyjs'),
-	paths        = {
-		scripts : [
-			'./layout/js/src/jquery.min.js',
-			'./layout/js/src/chart.js',
-			'./node_modules/bxslider/dist/jquery.bxslider.min.js',
-			'./layout/js/src/main.js'
-		]
-	};
+	autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('compile-less', function() {
 	gulp.src('./layout/less/style.less')
@@ -29,15 +20,4 @@ gulp.task('watch-less', function() {
 	gulp.watch('./layout/less/*.less', ['compile-less']);
 });
 
-gulp.task('compile-js', function() {
-	gulp.src(paths.scripts)
-		.pipe(concat('scripts.js'))
-		.pipe(uglify())
-		.pipe(gulp.dest('./layout/js/'));
-});
-
-gulp.task('watch-js', function() {
-	gulp.watch(paths.scripts, ['compile-js']);
-});
-
-gulp.task('default', ['compile-less', 'watch-less', 'compile-js', 'watch-js'] );  
+gulp.task('default', ['compile-less', 'watch-less'] );
---
+++ layout/js/main.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+
+	$('.products').bxSlider({
+		auto: true,
+		slideWidth: 270,
+		slideMargin: 30,
+		minSlides: 1,
+		maxSlides: 6,
+		moveSlides: 1,
+		pager: false
+	});
+
+	$('[data-href]').each(function (element) {
+		$(this).click(function () {
+			var modal = $(this).attr('data-href');
+            $('#fader').find('[data-modal="'+modal+'"]').show();
+			$('#fader').fadeIn(300, function () {
+                ("click keyup".split(" ")).forEach(function(e){
+                    document.addEventListener(e,closeModal,false);
+                });
+            });
+        })
+    });
+
+    function closeModal(event) {
+        if (event.keyCode === 27 || (event.type == 'click' && !$(event.target).closest('.modal').length)) {
+            $('#fader').fadeOut(300);
+            setTimeout(function () {
+                $('#fader .modal').hide();
+                ("click keyup".split(" ")).forEach(function(e){
+                    document.removeEventListener(e,closeModal,false);
+                });
+            }, 300);
+        }
+    }
+
+});
+
+function navbarToggle(element) {
+	$(element).slideToggle(300);
+}
